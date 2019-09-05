4a4e4097be27b834caab84e485b459cb347d7345
cs671660 <christian.scheiderer@ima-zlw-ifu.rwth-aachen.de>
2019-09-05 21:06:42 +0200

    Task: Saver
    New branch "Saver".
    Solve Todos. Commit regularly and detailed. All bad commits will be rejected in the final merge!
    Send merge request when branch is cleaned and documented.
    
    -sloumotion

---
+++ results/utils/saver.py
@@ -0,0 +1,6 @@
+#todo Saver module
+#todo
+# todo[?]:
+# todo[?]: Alle Statistik/Modell/... Speicher- und Ladevorgänge in eine Klasse packen
+# todo[?]: aufräumen. dokumentieren. nochmal aufräumen
+# todo[?]: git branch "saver"
